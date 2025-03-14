f72ebfdb27987d117ebd54db2ece96a9ff0fb54f
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-13 22:30:52 -0600

    Adding sessions and event details.

--- RedMist.Timing.UI/ViewModels/EventViewModel.cs
+++ RedMist.Timing.UI/ViewModels/EventViewModel.cs
@@ -2,22 +2,22 @@
 using CommunityToolkit.Mvvm.Messaging.Messages;
 using CommunityToolkit.Mvvm.Messaging;
 using RedMist.Timing.UI.Models;
+using RedMist.TimingCommon.Models;
 
 namespace RedMist.Timing.UI.ViewModels;
 
-public partial class EventViewModel : ObservableObject
+public partial class EventViewModel(Event eventModel) : ObservableObject
 {
-    public int EventId { get; set; }
-    [ObservableProperty]
-    private string organization = string.Empty;
-    [ObservableProperty]
-    private string name = string.Empty;
+    public Event EventModel { get; } = eventModel;
+    public string Name => EventModel.EventName;
+
+    public string Organization => EventModel.OrganizationName;
 
     public void SelectEvent(object eventViewModel)
     {
         if (eventViewModel is EventViewModel evm)
         {
-            var routerEvent = new RouterEvent { Path = "EventStatus", Data = evm.EventId };
+            var routerEvent = new RouterEvent { Path = "EventStatus", Data = EventModel };
             WeakReferenceMessenger.Default.Send(new ValueChangedMessage<RouterEvent>(routerEvent));
         }
     }
