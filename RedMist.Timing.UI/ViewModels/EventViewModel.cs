91c45bf75fcf96c75ef34306ed9c6b8c265cb8d0
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-16 15:08:48 -0600

    Styling improvements.

--- RedMist.Timing.UI/ViewModels/EventViewModel.cs
+++ RedMist.Timing.UI/ViewModels/EventViewModel.cs
@@ -1,17 +1,57 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Messaging.Messages;
+﻿using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
 using RedMist.Timing.UI.Models;
 using RedMist.TimingCommon.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
 
 namespace RedMist.Timing.UI.ViewModels;
 
-public partial class EventViewModel(Event eventModel) : ObservableObject
+public partial class EventViewModel : ObservableObject
 {
-    public Event EventModel { get; } = eventModel;
+    public Event EventModel { get; }
     public string Name => EventModel.EventName;
 
     public string Organization => EventModel.OrganizationName;
+
+    public Bitmap? OrganizationLogo
+    {
+        get
+        {
+            if (EventModel.OrganizationLogo is not null)
+            {
+                using MemoryStream ms = new(EventModel.OrganizationLogo);
+                return Bitmap.DecodeToWidth(ms, 55);
+                //return new Bitmap(ms);
+            }
+            return null;
+        }
+    }
+
+    public ObservableCollection<ScheduleDayViewModel> ScheduleDays { get; } = [];
+
+
+    public EventViewModel(Event eventModel)
+    {
+        EventModel = eventModel;
+        if (EventModel.Schedule != null)
+        {
+            var today = DateTime.Now.Day;
+            foreach (var day in EventModel.Schedule.Entries.GroupBy(e => e.DayOfEvent.Date))
+            {
+                if (today == day.Key.Day)
+                {
+                    ScheduleDays.Add(new ScheduleDayViewModel(day.Key, [.. day]));
+                }
+            }
+        }
+    }
+
 
     public void SelectEvent(object eventViewModel)
     {
