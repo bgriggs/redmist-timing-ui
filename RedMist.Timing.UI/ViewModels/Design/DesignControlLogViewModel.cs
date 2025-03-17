beb7f57aea33cd864e9987f5c12c4cb8eca47990
Brian Griggs <brian@bigmissionmotorsports.com>
2025-03-16 20:28:14 -0600

    Adding control log tab.

---
+++ RedMist.Timing.UI/ViewModels/Design/DesignControlLogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RedMist.TimingCommon.Models;
+
+namespace RedMist.Timing.UI.ViewModels.Design;
+
+public class DesignControlLogViewModel : ControlLogViewModel
+{
+    public DesignControlLogViewModel() : base(new Event(), new DesignHubClient())
+    {
+        
+    }
+}
