8cb409ae8d00704f6da5beb7b75255bff07e00a7
Brian Griggs <brian@bigmissionmotorsports.com>
2025-04-06 07:15:42 -0600

    Fix to view overlapping andriod pixel system bar.

--- RedMist.Timing.UI/Views/MainView.axaml.cs
+++ RedMist.Timing.UI/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using CommunityToolkit.Mvvm.Messaging;
 using RedMist.Timing.UI.Models;
 
@@ -10,6 +11,19 @@ public partial class MainView : UserControl, IRecipient<LauncherEvent>
     {
         InitializeComponent();
         WeakReferenceMessenger.Default.RegisterAll(this);
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        // Set the app to display edge-to-edge specifically for Android Pixel devices
+        // http://github.com/AvaloniaUI/Avalonia/issues/18544
+        var insetsManager = TopLevel.GetTopLevel(this)?.InsetsManager;
+        if (insetsManager is not null)
+        {
+            insetsManager.DisplayEdgeToEdge = true;
+        }
     }
 
     public void Receive(LauncherEvent message)
