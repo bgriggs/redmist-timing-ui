c6b592122dcf6fde54aa6c7f9cf6b821ffce03e6
Brian Griggs <brian.griggs@gmail.com>
2025-10-12 10:23:35 -0600

    Fix to iOS

--- RedMist.Timing.UI.iOS/Main.cs
+++ RedMist.Timing.UI.iOS/Main.cs
@@ -1,14 +1,7 @@
 ﻿using UIKit;
+using RedMist.Timing.UI.iOS;
 
-namespace RedMist.Timing.UI.iOS;
-
-public class Application
-{
-    // This is the main entry point of the application.
-    static void Main(string[] args)
-    {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
-        UIApplication.Main(args, null, typeof(AppDelegate));
-    }
-}
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIApplication.Main(args, null, typeof(AppDelegate));
